2e5a6118837408197362e5e8bf747490abb3b770
Max Kaye <m@xk.io>
2018-12-26 15:32:31 +1100

    Add test to check non-admins can't add admins

--- ethereum/contracts/Membership.sol
+++ ethereum/contracts/Membership.sol
@@ -1,9 +1,14 @@
-pragma solidity 0.5.2;
+pragma solidity 0.4.25;
+
+import "../node_modules/openzeppelin-solidity/contracts/access/Roles.sol";
 
 contract Membership {
+    using Roles for Roles.Role;
 
     uint256 constant UINT_48_MASK = 0xffffffffffff;
     uint256 constant UINT_32_MASK = 0xffffffff;
+
+    Roles.Role private admins;
 
     struct Member {
         uint256 packed;
@@ -12,21 +17,41 @@ contract Membership {
     mapping (address => Member) public members;
     address[] public memberList;
 
-    address public admin;
-
     event SetMember(address votingAddr, uint48 weight, uint48 startTime, uint48 endTime);
+    event AddAdmin(address admin);
+    event RevokeAdmin(address oldAdmin);
 
+    // modifier - authentication
 
     modifier onlyAdmin() {
-        require(msg.sender == admin, "msg.sender not admin");
+        require(admins.has(msg.sender), "NOT_ADMIN");
         _;
     }
 
-
+    // constructor
 
     constructor() public {
-        admin = msg.sender;
+        admins.add(msg.sender);
+        emit AddAdmin(msg.sender);
     }
+
+    // manage roles
+
+    function addAdmin(address _a) external onlyAdmin {
+        admins.add(_a);
+        emit AddAdmin(msg.sender);
+    }
+
+    function revokeAdminSelf() external onlyAdmin {
+        admins.remove(msg.sender);
+        emit RevokeAdmin(msg.sender);
+    }
+
+    function isAdmin(address a) external view returns (bool) {
+        return admins.has(a);
+    }
+
+    // membership list management
 
     function setMember(address votingAddr, uint32 weight, uint48 startTime, uint48 endTime) external onlyAdmin {
         if (members[votingAddr].packed == 0) {
@@ -40,6 +65,8 @@ contract Membership {
         (weight, startTime, endTime) = unpack(members[voter].packed);
     }
 
+    // utils
+
     function pack(uint32 weight, uint48 start, uint48 end) internal pure returns (uint256) {
         return (uint256(weight) << 96) | (uint256(start) << 48) | uint256(end);
     }
@@ -50,6 +77,7 @@ contract Membership {
         end = uint48(packed & UINT_48_MASK);
     }
 
+    // erc20 shim
 
     function balanceOf(address v) external view returns (uint256) {
         uint32 weight; uint48 start; uint48 end;
--- ethereum/test/membership.ts
+++ ethereum/test/membership.ts
@@ -1,22 +1,40 @@
 const Membership = artifacts.require("Membership")
 
-contract("Add member", async ([a1]) => {
+contract("Add member", async ([a1, a2, a3]) => {
     console.log("Address1", a1)
 
     it("should add a member with some weight and timestamps", async () => {
         let membership = await Membership.deployed();
-        console.log(membership.address);
         let {weight, startTime, endTime} = await membership.getMember.call(a1);
 
         assert.equal(weight, 0);
         assert.equal(startTime, 0);
         assert.equal(endTime, 0);
 
-        const setMemberResult = await membership.setMember(a1, 1337, 1, 2000000000);
+        await membership.setMember(a1, 1337, 1, 2000000000);
         ({weight, startTime, endTime} = await membership.getMember.call(a1));
 
         assert.equal(weight, 1337);
         assert.equal(startTime, 1);
         assert.equal(endTime, 2000000000);
     })
+
+    it("should allow adding and revoking admin status", async () => {
+        let membership = await Membership.deployed();
+        assert.equal(await membership.isAdmin.call(a2), false, 'admin check before grant');
+
+        await membership.addAdmin(a2, {from: a1});
+        assert.equal(await membership.isAdmin.call(a2), true, 'admin check post grant');
+
+        await membership.revokeAdminSelf({from: a1})
+        assert.equal(await membership.isAdmin.call(a1), false, 'admin check post grant');
+
+        try {
+            await membership.addAdmin(a3, {from: a3});
+            throw new Error("addAdmin succeeded from bad address!")
+        } catch (e) {
+            assert.equal(e.reason, "NOT_ADMIN", "failed addAdmin from bad addr check")
+        }
+    })
 });
+
